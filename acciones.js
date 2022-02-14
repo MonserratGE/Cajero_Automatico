1bdec5e278c9bac5cb18a75498fbcefeb2e012f0
MonserratGE <monserrat.garcia.espinoza32@gmail.com>
2022-02-14 12:18:10 -0600

    Creacion de archivo

---
+++ acciones.js
@@ -0,0 +1,52 @@
+
+  //let retiro = document.getElementById("cantidad").value;
+
+  /*let enter = document.getElementById("btnenter");
+  enter.addEventListener('click', btnenter);
+  function btnenter() {
+    let saldo = 990;
+    let deA500 = 0;
+    let deA200 = 0;
+    let deA100 = 0;
+    let deA50 = 0;
+    let deA10 = 0;
+    let b500 = 500;
+    let b200 = 200;
+    let b100 = 100;
+    let b50 = 50;
+    let b10 = 10;
+    while (retiro > saldo || retiro == 0) {
+      //retiro = parseInt(prompt("Cuanto vas a retirar")); || retiro % 50 != 0
+      retiro = document.getElementById("cantidad").value;
+    }
+
+    saldo = saldo - retiro;
+    deA500 = parseInt(retiro / b500);
+    retiro = retiro - deA500 * b500;
+    deA200 = parseInt(retiro / b200);
+    retiro = retiro - deA200 * b200;
+    deA100 = parseInt(retiro / b100);
+    retiro = retiro - deA100 * b100;
+    deA50 = parseInt(retiro / b50);
+    retiro = retiro - deA50 * b50;
+    deA10 = parseInt(retiro / b10);
+    retiro = retiro - deA10 * b10;
+    document.write(deA500 + " de 500" + "<br>");
+    document.write(deA200 + " de 200" + "<br>");
+    document.write(deA100 + " de 100" + "<br>");
+    document.write(deA50 + " de 50" + "<br>");
+    document.write(deA10 + " de 10" + "<br>");
+    document.write("Tu saldo actual es: " + saldo);
+  }
+
+  let enter = document.getElementById("btnenter");
+  enter.addEventListener('click', btnenter);
+  function btnenter(){
+    let saldo = 990;
+    let deposito = parseInt(document.getElementById("cantidad").value);
+    saldo = saldo + deposito;
+    document.write("Tu saldo actual es: " + saldo);
+  }*/
+
+  
+
